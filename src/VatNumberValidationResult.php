58e86009bfccc752c5f2f0d2a53b4fbec5d7e413
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-26 19:00:10 +0400

    [ECVS-2] added attributes.

--- src/VatNumberValidationResult.php
+++ src/VatNumberValidationResult.php
@@ -4,4 +4,9 @@ namespace rocketfellows\ViesVatValidationInterface;
 
 class VatNumberValidationResult
 {
+    private $vatNumber;
+    private $requestDate;
+    private $isValid;
+    private $name;
+    private $address;
 }
