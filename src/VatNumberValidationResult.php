a7b194c1894517223ffb7a5feedbcf932846b70f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-26 19:01:45 +0400

    [ECVS-2] implemented constructor.

--- src/VatNumberValidationResult.php
+++ src/VatNumberValidationResult.php
@@ -9,4 +9,18 @@ class VatNumberValidationResult
     private $isValid;
     private $name;
     private $address;
+
+    public function __construct(
+        VatNumber $vatNumber,
+        string $requestDate,
+        bool $isValid,
+        ?string $name = null,
+        ?string $address = null
+    ) {
+        $this->vatNumber = $vatNumber;
+        $this->requestDate = $requestDate;
+        $this->isValid = $isValid;
+        $this->name = $name;
+        $this->address = $address;
+    }
 }
