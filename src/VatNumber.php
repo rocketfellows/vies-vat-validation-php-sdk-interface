27d945ccbece8cfa7cee020889a01da33eae5260
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-26 18:03:02 +0400

    [ECVS-2] added constructor.

--- src/VatNumber.php
+++ src/VatNumber.php
@@ -6,4 +6,10 @@ class VatNumber
 {
     private $countryCode;
     private $vatNumber;
+
+    public function __construct(string $countryCode, string $vatNumber)
+    {
+        $this->countryCode = $countryCode;
+        $this->vatNumber = $vatNumber;
+    }
 }
