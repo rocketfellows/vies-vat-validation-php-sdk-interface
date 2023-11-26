27311a5ec71352a256b53876313af38d4da398a7
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-26 18:36:09 +0400

    [ECVS-2] added test case: request date set, vat is valid, name set, address set.

--- tests/unit/VatNumberValidationResultTest.php
+++ tests/unit/VatNumberValidationResultTest.php
@@ -3,10 +3,23 @@
 namespace rocketfellows\ViesVatValidationInterface\tests\unit;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ViesVatValidationInterface\VatNumber;
 
 /**
  * @group vies-vat-validation-interface
  */
 class VatNumberValidationResultTest extends TestCase
 {
+    public function getVatNumberValidationResultProvidedData(): array
+    {
+        return [
+            'request date set, vat is valid, name set, address set' => [
+                'vatNumber' => new VatNumber('DE', 'foo'),
+                'requestDate' => '2023-11-26 23:23:23',
+                'isValid' => true,
+                'name' => 'some name',
+                'address' => 'some address',
+            ],
+        ];
+    }
 }
