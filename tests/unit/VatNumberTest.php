f64e3ce30ba109675f64038275109b189b431a5f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-26 17:48:50 +0400

    [ECVS-2] created vat number test.

---
+++ tests/unit/VatNumberTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationInterface\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-interface
+ */
+class VatNumberTest extends TestCase
+{
+}
