44c2c6881d50d8656bbed80a1ec74a7e340e6eac
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-28 15:10:43 +0400

    [ECVS-2] added contract exception: InvalidRequesterInfoServiceException.

--- src/VatNumberValidationServiceInterface.php
+++ src/VatNumberValidationServiceInterface.php
@@ -3,11 +3,13 @@
 namespace rocketfellows\ViesVatValidationInterface;
 
 use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidRequesterInfoServiceException;
 
 interface VatNumberValidationServiceInterface
 {
     /**
      * @throws InvalidInputServiceException
+     * @throws InvalidRequesterInfoServiceException
      */
     public function validateVat(VatNumber $vatNumber): VatNumberValidationResult;
 }
