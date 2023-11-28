48866ebddcbcdcd91c14c7923d4a21b49ffb6055
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-28 15:10:59 +0400

    [ECVS-2] added contract exception: ServiceUnavailableException.

--- src/VatNumberValidationServiceInterface.php
+++ src/VatNumberValidationServiceInterface.php
@@ -4,12 +4,14 @@ namespace rocketfellows\ViesVatValidationInterface;
 
 use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
 use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidRequesterInfoServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceUnavailableException;
 
 interface VatNumberValidationServiceInterface
 {
     /**
      * @throws InvalidInputServiceException
      * @throws InvalidRequesterInfoServiceException
+     * @throws ServiceUnavailableException
      */
     public function validateVat(VatNumber $vatNumber): VatNumberValidationResult;
 }
