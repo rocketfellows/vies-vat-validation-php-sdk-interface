08d206c9584a25411a0fc5499f8671e397ede7ee
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-11-28 15:10:28 +0400

    [ECVS-2] added contract exception: InvalidInputServiceException.

--- src/VatNumberValidationServiceInterface.php
+++ src/VatNumberValidationServiceInterface.php
@@ -2,7 +2,12 @@
 
 namespace rocketfellows\ViesVatValidationInterface;
 
+use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
+
 interface VatNumberValidationServiceInterface
 {
+    /**
+     * @throws InvalidInputServiceException
+     */
     public function validateVat(VatNumber $vatNumber): VatNumberValidationResult;
 }
