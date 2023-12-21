616f8d98460e94ec1e1b7b9eeb96594c66c4f2fd
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-21 16:42:15 +0400

    [ECVS-6] handle SERVICE_UNAVAILABLE fault code.

--- src/FaultCodeExceptionFactory.php
+++ src/FaultCodeExceptionFactory.php
@@ -3,6 +3,7 @@
 namespace rocketfellows\ViesVatValidationInterface;
 
 use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceUnavailableException;
 use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
 use rocketfellows\ViesVatValidationInterface\exceptions\VatNumberValidationException;
 
@@ -14,6 +15,8 @@ class FaultCodeExceptionFactory
         switch ($faultCode) {
             case FaultCodes::INVALID_INPUT:
                 return new InvalidInputServiceException();
+            case FaultCodes::SERVICE_UNAVAILABLE:
+                return new ServiceUnavailableException();
             default:
                 return new UnknownServiceErrorException();
         }
