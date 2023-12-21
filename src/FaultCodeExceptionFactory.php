1d87f4da3aa7bea05180cbbe933b8ea8d2f31228
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-21 15:07:24 +0400

    [ECVS-6] added create function stub.

--- src/FaultCodeExceptionFactory.php
+++ src/FaultCodeExceptionFactory.php
@@ -2,6 +2,15 @@
 
 namespace rocketfellows\ViesVatValidationInterface;
 
+use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
+use rocketfellows\ViesVatValidationInterface\exceptions\VatNumberValidationException;
+
 class FaultCodeExceptionFactory
 {
+    public function create(string $faultCode): VatNumberValidationException
+    {
+        // TODO: implement
+
+        return new UnknownServiceErrorException();
+    }
 }
