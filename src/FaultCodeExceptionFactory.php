7063fda87c5ab9738893967fe2d5ebf27246fc34
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-21 16:42:37 +0400

    [ECVS-6] handle MS_UNAVAILABLE fault code.

--- src/FaultCodeExceptionFactory.php
+++ src/FaultCodeExceptionFactory.php
@@ -3,6 +3,7 @@
 namespace rocketfellows\ViesVatValidationInterface;
 
 use rocketfellows\ViesVatValidationInterface\exceptions\service\InvalidInputServiceException;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\MSUnavailableServiceException;
 use rocketfellows\ViesVatValidationInterface\exceptions\service\ServiceUnavailableException;
 use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
 use rocketfellows\ViesVatValidationInterface\exceptions\VatNumberValidationException;
@@ -17,6 +18,8 @@ class FaultCodeExceptionFactory
                 return new InvalidInputServiceException();
             case FaultCodes::SERVICE_UNAVAILABLE:
                 return new ServiceUnavailableException();
+            case FaultCodes::MS_UNAVAILABLE:
+                return new MSUnavailableServiceException();
             default:
                 return new UnknownServiceErrorException();
         }
