2685e966b657fe8a4b533ee9b0349bbaa245a088
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-23 14:43:06 +0400

    [ECVS-6] covered default create exception.

--- tests/unit/exceptions/service/UnknownServiceErrorExceptionTest.php
+++ tests/unit/exceptions/service/UnknownServiceErrorExceptionTest.php
@@ -3,10 +3,40 @@
 namespace rocketfellows\ViesVatValidationInterface\tests\unit\exceptions\service;
 
 use PHPUnit\Framework\TestCase;
+use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
 
 /**
  * @group vies-vat-validation-interface
  */
 class UnknownServiceErrorExceptionTest extends TestCase
 {
+    /**
+     * @dataProvider getDefaultCreateExceptionProvidedData
+     */
+    public function testDefaultCreateException(array $exceptionData, array $expectedExceptionData): void
+    {
+        $exception = new UnknownServiceErrorException($exceptionData['faultCode']);
+
+        $this->assertEquals($expectedExceptionData['expectedFaultCode'], $exception->getFaultCode());
+        $this->assertEquals($expectedExceptionData['expectedMessage'], $exception->getMessage());
+        $this->assertEquals($expectedExceptionData['expectedCode'], $exception->getCode());
+        $this->assertEquals($expectedExceptionData['expectedPrevException'], $exception->getPrevious());
+    }
+
+    public function getDefaultCreateExceptionProvidedData(): array
+    {
+        return [
+            'fault code empty' => [
+                'exceptionData' => [
+                    'faultCode' => '',
+                ],
+                'expectedExceptionData' => [
+                    'expectedFaultCode' => '',
+                    'expectedMessage' => '',
+                    'expectedCode' => 0,
+                    'expectedPrevException' => null,
+                ],
+            ],
+        ];
+    }
 }
