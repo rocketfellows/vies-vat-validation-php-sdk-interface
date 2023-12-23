786a7fdf591fb890e019a54eee370843dafd6ede
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-23 14:47:47 +0400

    [ECVS-6] fixed test.

--- tests/unit/exceptions/service/UnknownServiceErrorExceptionTest.php
+++ tests/unit/exceptions/service/UnknownServiceErrorExceptionTest.php
@@ -2,6 +2,7 @@
 
 namespace rocketfellows\ViesVatValidationInterface\tests\unit\exceptions\service;
 
+use Exception;
 use PHPUnit\Framework\TestCase;
 use rocketfellows\ViesVatValidationInterface\exceptions\service\UnknownServiceErrorException;
 
@@ -52,11 +53,16 @@ class UnknownServiceErrorExceptionTest extends TestCase
     }
 
     /**
-     * @dataProvider getDefaultCreateExceptionProvidedData
+     * @dataProvider getCreateExceptionProvidedData
      */
     public function testCreateException(array $exceptionData, array $expectedExceptionData): void
     {
-        $exception = new UnknownServiceErrorException($exceptionData['faultCode']);
+        $exception = new UnknownServiceErrorException(
+            $exceptionData['faultCode'],
+            $exceptionData['message'],
+            $exceptionData['code'],
+            $exceptionData['prevException']
+        );
 
         $this->assertEquals($expectedExceptionData['expectedFaultCode'], $exception->getFaultCode());
         $this->assertEquals($expectedExceptionData['expectedMessage'], $exception->getMessage());
@@ -66,6 +72,8 @@ class UnknownServiceErrorExceptionTest extends TestCase
 
     public function getCreateExceptionProvidedData(): array
     {
+        $prevException = new Exception();
+
         return [
             'fault code empty, message empty, default code, prev exception not set' => [
                 'exceptionData' => [
@@ -79,6 +87,20 @@ class UnknownServiceErrorExceptionTest extends TestCase
                     'expectedMessage' => '',
                     'expectedCode' => 0,
                     'expectedPrevException' => null,
+                ],
+            ],
+            'fault code not empty, message not empty, not default code, prev exception set' => [
+                'exceptionData' => [
+                    'faultCode' => 'foo',
+                    'message' => 'foo bar',
+                    'code' => 1,
+                    'prevException' => $prevException,
+                ],
+                'expectedExceptionData' => [
+                    'expectedFaultCode' => 'foo',
+                    'expectedMessage' => 'foo bar',
+                    'expectedCode' => 1,
+                    'expectedPrevException' => $prevException,
                 ],
             ],
         ];
