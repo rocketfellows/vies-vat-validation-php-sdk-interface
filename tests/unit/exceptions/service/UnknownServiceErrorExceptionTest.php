54468addd427929994a03c71ac4c7f56acfcd50a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-23 14:38:14 +0400

    [ECVS-6] created test.

---
+++ tests/unit/exceptions/service/UnknownServiceErrorExceptionTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace rocketfellows\ViesVatValidationInterface\tests\unit\exceptions\service;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group vies-vat-validation-interface
+ */
+class UnknownServiceErrorExceptionTest extends TestCase
+{
+}
