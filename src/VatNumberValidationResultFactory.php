9c89aad49104820e628f0fedadc98372f2bf6e46
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-30 14:05:09 +0400

    [ECVS-7] added create from object scratch.

--- src/VatNumberValidationResultFactory.php
+++ src/VatNumberValidationResultFactory.php
@@ -2,6 +2,13 @@
 
 namespace rocketfellows\ViesVatValidationInterface;
 
+use stdClass;
+
 class VatNumberValidationResultFactory
 {
+    public function createFromObject(stdClass $rawData): VatNumberValidationResult
+    {
+        // TODO: implement
+        return VatNumberValidationResult::create();
+    }
 }
