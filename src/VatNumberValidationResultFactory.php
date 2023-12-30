a11c0c178407d222b4bbc9b7a0f1d232ccd4f509
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-12-30 14:08:21 +0400

    [ECVS-7] added expected attribute names consts.

--- src/VatNumberValidationResultFactory.php
+++ src/VatNumberValidationResultFactory.php
@@ -6,6 +6,13 @@ use stdClass;
 
 class VatNumberValidationResultFactory
 {
+    private const ATTRIBUTE_NAME_COUNTRY_CODE = 'countryCode';
+    private const ATTRIBUTE_NAME_VAT_NUMBER = 'vatNumber';
+    private const ATTRIBUTE_NAME_REQUEST_DATE = 'requestDate';
+    private const ATTRIBUTE_NAME_VALIDATION_FLAG = 'valid';
+    private const ATTRIBUTE_NAME_VAT_OWNER_NAME = 'name';
+    private const ATTRIBUTE_NAME_VAT_OWNER_ADDRESS = 'address';
+
     public function createFromObject(stdClass $rawData): VatNumberValidationResult
     {
         // TODO: implement
